45af2bd98eadb4f77b4454e9e70d52fafb6d5cc2
Sanchay Kumar <sanchaykumar9@gmail.com>
2021-11-13 19:35:19 +0530

    update polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,8 +2,6 @@
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use
-# polybar-msg cmd quit
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
