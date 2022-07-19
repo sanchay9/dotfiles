054cbf0ea97d52154dbb4edcf48e3ba11c782a69
Sanchay Kumar <53908642+sanchay9@users.noreply.github.com>
2022-07-20 00:47:56 +0530

    Delete nvim directory

--- nvim/lua/plugins/indentline.lua
+++
@@ -1,9 +0,0 @@
-vim.opt.listchars = {
-    eol = "↴",
-    tab = "   ",
-}
-
-vim.g.indent_blankline_char = "▏"  --'▏', '┊', '|', '¦', '┆'
-vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
-vim.g.indent_blankline_filetype_exclude = { 'gitcommit', 'help', 'packer', 'markdown', 'txt', 'startify', 'vimwiki', 'undotree' }
-vim.g.indent_blankline_bufname_exclude = { 'Input', 'Output' }
